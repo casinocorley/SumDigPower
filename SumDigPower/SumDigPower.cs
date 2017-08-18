3bd153f3a23e9b4e5c2b8313b0002e595377c47d
Casino <ccorley@healthgrades.com>
2017-08-18 09:45:04 -0600

    Initial Checkin

---
+++ SumDigPower/SumDigPower.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class SumDigPower
+{
+
+    public static long[] SumDigPow(long a, long b)
+    {
+        if (a > b)
+            throw new ArgumentException();
+
+        var length = b - a + 1;
+
+        var numbers = Enumerable
+            .Range(a, length)
+            .Where(x => IsSumDigPower(x))
+            .ToArray();
+
+        return numbers;
+    }
+
+    public static bool IsSumDigPower(long a)
+    {
+        var digits = a
+            .ToDigits()
+            .ToList();
+
+        var sum = (long) 0;
+
+        for (int i = 0; i < digits.Count(); i++)
+            sum += (long)Math.Pow(digits[i], i + 1);
+
+        return a == sum;
+    }
+}
+
+public static class Extensions
+{
+    public static IEnumerable<long> ToDigits(this long a)
+    {
+        return a.ToString()
+            .Select(x => Int64.Parse(x.ToString()));
+    }
+}
+
+/// <summary>
+/// Copied from https://github.com/Microsoft/referencesource/blob/master/System.Core/System/Linq/Enumerable.cs
+/// and modified for long
+/// </summary>
+public static class Enumerable
+{
+    public static IEnumerable<long> Range(long start, long count)
+    {
+        long max = ((long)start) + count - 1;
+        if (count < 0 || max > Int64.MaxValue)
+            throw new ArgumentOutOfRangeException("count");
+
+        return RangeIterator(start, count);
+    }
+
+    static IEnumerable<long> RangeIterator(long start, long count)
+    {
+        for (int i = 0; i < count; i++)
+            yield return start + i;
+    }
+}
+
---
+++ SumDigPower/SumDigPowerTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+
+
+[TestFixture]
+public static class SumDigPowerTests
+{
+    [Test]
+    public static void DoesntErrorWhenRangeIsValid()
+    {
+        Assert.DoesNotThrow(() => SumDigPower.SumDigPow(1, 2));
+    }
+
+    [Test]
+    public static void DoesErrorWhenRangeIsInvalid()
+    {
+        Assert.Throws<ArgumentException>(() => SumDigPower.SumDigPow(2, 1));
+    }
+    [Test]
+    public static void DoesntErrorWhenRangeIsSameNumber()
+    {
+        Assert.DoesNotThrow(() => SumDigPower.SumDigPow(1, 1));
+    }
+
+    [Test]
+    public static void Number89IsValid()
+    {
+        // Arrange
+
+        // Act
+        var isValid = SumDigPower.IsSumDigPower(89);
+
+        // Assert
+        Assert.IsTrue(isValid);
+    }
+
+    [Test]
+    public static void Number10IsInvalid()
+    {
+        // Arrange
+
+        // Act
+        var isValid = SumDigPower.IsSumDigPower(10);
+
+        // Assert
+        Assert.IsFalse(isValid);
+    }
+
+    private static string Array2String(long[] list)
+    {
+        return "[" + string.Join(", ", list) + "]";
+    }
+    private static void testing(long a, long b, long[] res)
+    {
+        Assert.AreEqual(Array2String(res),
+                        Array2String(SumDigPower.SumDigPow(a, b)));
+    }
+    [Test]
+    public static void test1()
+    {
+        Console.WriteLine("Basic Tests SumDigPow");
+        testing(1, 10, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        testing(1, 100, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 89 });
+        testing(10, 100, new long[] { 89 });
+        testing(90, 100, new long[] { });
+        testing(90, 150, new long[] { 135 });
+        testing(50, 150, new long[] { 89, 135 });
+        testing(10, 150, new long[] { 89, 135 });
+
+    }
+}
+
